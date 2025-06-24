77dec4076596a094607f056b3c45672da4c95918
Javier009 <38794360+Javier009@users.noreply.github.com>
2025-06-24 11:34:00 -0700

    Read Json files function

--- SCD2_process/write_to_staging/main.py
+++ SCD2_process/write_to_staging/main.py
@@ -13,7 +13,16 @@ GCS_client = storage.Client(project=PROJECT_ID)
 def read_json_file(request:Request):
 
     try:
+        # Try to get file_name from query string
         file_name = request.args.get("file_name")
+
+        # If not found, try to get it from JSON body
+        if not file_name:
+            data = request.get_json(silent=True)
+            file_name = data.get("file_name") if data else None
+        if not file_name:
+            return "Missing 'file_name' parameter in query or body", 400
+        
         print(file_name)
         bucket = GCS_client.bucket(RAW_DATA_BUCKET)
         blob = bucket.blob(file_name)
@@ -21,5 +30,6 @@ def read_json_file(request:Request):
         records = [json.loads(line) for line in json_string.strip().split('\n')]
         print(records[0:3])
         return  f"Succesfully read file {file_name}", 200
+    
     except Exception as e:
         return f"An error occurred while reading file {file_name}: {e}", 500
