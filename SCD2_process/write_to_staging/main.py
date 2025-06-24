3602785a06e24ac5a1327fc5d4da071dfae42865
Javier009 <38794360+Javier009@users.noreply.github.com>
2025-06-24 11:05:01 -0700

    Read Json files function

--- SCD2_process/write_to_staging/main.py
+++ SCD2_process/write_to_staging/main.py
@@ -1,7 +1,5 @@
 from flask import Request
-from datetime import datetime
 import json
-# import argparse
 
 # Import Cloud Libraries
 from google.cloud import storage
@@ -10,17 +8,17 @@ from google.cloud import storage
 PROJECT_ID = 'gcp-workflows-463821'
 RAW_DATA_BUCKET = 'e-commerce-project-raw'
 
-
-
-
 GCS_client = storage.Client(project=PROJECT_ID)
 
 def read_json_file(request:Request):
 
-    file_name = request.args.get("file_name")
-    bucket = GCS_client.bucket(RAW_DATA_BUCKET)
-    blob = bucket.blob(file_name)
-    json_string = blob.download_as_text(encoding="utf-8")
-    records = [json.loads(line) for line in json_string.strip().split('\n')]
-    
-    print(records[0:3])
+    try:
+        file_name = request.args.get("file_name")
+        bucket = GCS_client.bucket(RAW_DATA_BUCKET)
+        blob = bucket.blob(file_name)
+        json_string = blob.download_as_text(encoding="utf-8")
+        records = [json.loads(line) for line in json_string.strip().split('\n')]
+        print(records[0:3])
+        return 200, f"Succesfully read file {file_name}"
+    except Exception as e:
+        return 500, f"An error occurred while reading file {file_name}: {e}"
